369ccb86594bab46fe23e410c3888f82bc49d2a2
SteelPlated <mix.singatulin@yandex.ru>
2022-05-18 18:41:08 +0500

    4

---
+++ lesson2/Examples010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array={1,24,35,88,44,57,66,77,88};
+
+int n=array.Length;
+int find = 88;
+
+int index = 0;
+ while (index<n)
+ {
+     if(array[index]==find)
+     {
+         Console.WriteLine(index);
+         break;
+     }
+     //index=index+ 1;
+     index++;
+ }
