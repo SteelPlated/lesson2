4d00d25a11dff5afc2894ddf830a09bf7ef8c0e9
SteelPlated <mix.singatulin@yandex.ru>
2022-05-18 14:50:14 +0500

    1

---
+++ lesson2/Examples0008/Program.cs
@@ -0,0 +1,32 @@
+﻿//Найдем наибольшее из 9
+
+int Max(int arg1, int arg2, int arg3)
+{
+    int result =arg1;
+    if(arg2>result) result=arg2;
+    if(arg3>result) result=arg3;
+    return result;
+}
+
+int a1 = 13;
+int a2 = 14;
+int a3 = 1996;
+int b1 = 66;
+int b2 = 2343;
+int b3 = 324;
+int c1 = 13243;
+int c2 = 1253;
+int c3 = 123;
+
+int max1 = Max(a1,b1,c1);
+int max2 = Max(a2,b2,c2);
+int max3 = Max(a3,b3,c3);
+int max = Max(max1, max2, max3);
+
+// Так же можно сделать так
+// int max1 = Max(
+//   Max(a1,b1,c1),
+//   Max(a2,b2,c2),
+//   Max(a3,b3,c3));
+
+Console.WriteLine(max);
---
+++ lesson2/Examples008_IntroMethod/Program.cs
@@ -0,0 +1,27 @@
+﻿//Найдем наибольшее из 9
+
+int a1 = 13;
+int a2 = 14;
+int a3 = 199996;
+int b1 = 66;
+int b2 = 2343;
+int b3 = 324;
+int c1 = 13243;
+int c2 = 1253;
+int c3 = 123;
+
+int max = a1;
+if(b1>max) max=b1;
+if(c1>max) max=c1;
+
+if(a2>max) max=a2;
+if(b2>max) max=b2;
+if(c2>max) max=c2;
+
+if(a3>max) max=a3;
+if(b3>max) max=b3;
+if(c3>max) max=c3;
+
+Console.WriteLine(max);
+
+
